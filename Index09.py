bde09530304cac81ee495bb991b1059707f31d4c
shoxrux <shoxruxnorqoziyev2004@gmail.com>
2023-07-22 17:54:46 +0500

    int turiga utkazish

--- Index09.py
+++ Index09.py
@@ -6,4 +6,26 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    if s[0]=="0":
+        return 0
+    if s[0]=="1":
+        return 1
+    if s[0]=="2":
+        return 2
+    if s[0]=="3":
+        return 3
+    if s[0]=="4":
+        return 4
+    if s[0]=="5":
+        return 5
+    if s[0]=="6":
+        return 6
+    if s[0]=="7":
+        return 7
+    if s[0]=="8":
+        return 8
+    if s[0]=="9":
+        return 9
+    return False
+print(main("7"))
+print(main("k"))
